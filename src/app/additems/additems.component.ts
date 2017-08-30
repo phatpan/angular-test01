72e1c825e291564866ecca4da269c70b0cbe8386
phatpan <phat.pan@gmail.com>
2017-08-30 22:09:57 +0700

    display data when edit

--- src/app/additems/additems.component.ts
+++ src/app/additems/additems.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AngularFireDatabase, FirebaseListObservable} from 'angularfire4/database'
+import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire4/database'
 import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -9,27 +9,43 @@ import {Router, ActivatedRoute} from '@angular/router';
   styleUrls: ['./additems.component.css']
 })
 export class AdditemsComponent implements OnInit {
-  title = ""; 
-  items: FirebaseListObservable<any[]>;
+  title:string; 
+  employee: FirebaseObjectObservable<any>;
+  id:string;
+  firstname:string;
+  item: any= {firstname: ""};
 
   constructor(
     private db:AngularFireDatabase, 
     private router: Router, 
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute) {
+      this.id = this.route.snapshot.paramMap.get("id");
+      this.title = "Add employee";
+      if(this.id){
+        this.title = "Edit employee";
+        this.employee = this.db.object('employees/'+ this.id,{ preserveSnapshot: true });
+        this.employee.subscribe(snapshot =>{
+          this.item = snapshot.val();
+        });
+      }
+     }
 
   ngOnInit() {
-    let id = this.route.snapshot.paramMap.get("id");
-    this.title = "Add employee";
-    if(id){
-      this.title = "Edit employee";
-    }
+    
   }
 
   saveItems(data: NgForm){
     if(data.valid){
-      this.db.list("/employees").push(data.value);
-      this.router.navigate(['/']);
+      if(this.id){
+        this.db.list("/employees").update(this.id,data.value).then(this.goToHome);
+      }else{
+        this.db.list("/employees").push(data.value).then(this.goToHome);
+      }
     }
+  }
+
+  goToHome=()=>{
+    this.router.navigate(['/']);
   }
 
 }
